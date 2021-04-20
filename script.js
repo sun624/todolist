8a0fdc3bcabbc1c3431a5acc0e22184c78c05bc0
sun624 <67940809+sun624@users.noreply.github.com>
2021-04-19 19:06:06 -0700

    add resetting

--- script.js
+++ script.js
@@ -18,65 +18,70 @@ async function createDestination(event) {
   event.preventDefault();
 
   //get user info
-  var name = event.target["destination_name"].value;
-  var location = event.target["destination_location"].value;
-  var photo = await getPhotoUrl(event.target["destination_name"].value);
-  var description = event.target["description"].value;
+  const name = event.target["destination_name"].value;
+  const location = event.target["destination_location"].value;
+  const photo = await getPhotoUrl(event.target["destination_name"].value);
+  const description = event.target["description"].value;
+
+  //reset user input
+  event.target["destination_name"].value="";
+  event.target["destination_location"].value="";
+  event.target["description"].value="";
 
   //create a card
-  var newCard = document.createElement("div");
+  const newCard = document.createElement("div");
   newCard.setAttribute("class", "card");
-  newCard.style.width = "18rem";
+  newCard.style.width = "15rem";
   newCard.style.margin = "20px";
   document.querySelector(".destination_container").appendChild(newCard);
 
   //create the image
-  var image = document.createElement("img");
+  const image = document.createElement("img");
   image.setAttribute("class", "card-img-top");
   const fallBackUrl =
     "https://cavchronicle.org/wp-content/uploads/2018/03/top-travel-destination-for-visas-900x504.jpg";
-  let url = photo.length === 0 ? fallBackUrl : photo;
+  const url = photo.length === 0 ? fallBackUrl : photo;
   image.setAttribute("src", url);
   newCard.appendChild(image);
 
   //create card body
-  var cardBody = document.createElement("div");
+  const cardBody = document.createElement("div");
   cardBody.setAttribute("class", "card-body");
   newCard.appendChild(cardBody);
 
   //create Destination name (card-title)
-  var cardTitle = document.createElement("h5");
+  const cardTitle = document.createElement("h5");
   cardTitle.setAttribute("class", "card-title");
   cardTitle.innerHTML = name;
   cardBody.appendChild(cardTitle);
 
   //create Destination location(card-subtitle)
-  var cardSubtitle = document.createElement("h6");
+  const cardSubtitle = document.createElement("h6");
   cardSubtitle.setAttribute("class", "card-subtitle");
   cardSubtitle.innerHTML = location;
   cardSubtitle.style.color = "gray";
   cardBody.appendChild(cardSubtitle);
 
   //create description
-  var cardDescription = document.createElement("p");
+  const cardDescription = document.createElement("p");
   cardDescription.setAttribute("class", "card-text");
   cardDescription.innerHTML = description;
   cardBody.appendChild(cardDescription);
 
   //create button container
-  var buttonContainer = document.createElement("div");
+  const buttonContainer = document.createElement("div");
   buttonContainer.setAttribute("class", "button_container");
   cardBody.appendChild(buttonContainer);
 
   //create edit button
-  var editButton = document.createElement("button");
+  const editButton = document.createElement("button");
   editButton.setAttribute("class", "btn btn-warning");
   editButton.innerHTML = "Edit";
   editButton.addEventListener("click", editDestinaton);
   buttonContainer.appendChild(editButton);
 
   //create remove button
-  var removeButton = document.createElement("button");
+  const removeButton = document.createElement("button");
   removeButton.setAttribute("class", "btn btn-danger");
   removeButton.innerHTML = "Remove";
   removeButton.addEventListener("click", removeDestinaton);
